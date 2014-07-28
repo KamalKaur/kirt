0fc3dd3bed20fb5b2834894af6b513f363f5bb82
Kamal <kamal.kaur188@gmail.com>
2014-07-28 23:04:11 +0530

    Forms in single template

--- src/views.py
+++ src/views.py
@@ -23,15 +23,28 @@ def index(request):
 		if request.method == "POST":
 			form = AdvanceForm(request.POST)
 			if form.is_valid:
-				form.save()
+				form.save()			
 		else:
 			form = AdvanceForm()
-			return render(request,'src/form.html',{'AdvanceForm':form})	
 
+		if request.method == "POST":
+			form2 = MonthlyAttendanceForm(request.POST)
+			if form2.is_valid:
+				form2.save()
+		else:
+			form2 = MonthlyAttendanceForm()
+			
 
+		if request.method == "POST":
+			form3 = PaidSalaryForm(request.POST)
+			if form3.is_valid:
+				form3.save()
+		else:
+			form3 = PaidSalaryForm()
+		return render(request,'src/form.html',{'AdvanceForm':form, 'MonthlyAttendanceForm': form2, 'PaidSalaryForm':form3})
 	
 
-def addworker(request):
+"""def addworker(request):
 	if request.method == "POST":
 		form = WorkerDetailForm(request.POST)
 		if form.is_valid:
@@ -39,25 +52,25 @@ def addworker(request):
 			return HttpResponse("Submitted oye :D ")
 	else:
 		form = WorkerDetailForm()
-		return render(request,'src/form.html', {'WorkerDetailForm':form})
+		return render(request,'src/form.html', {'WorkerDetailForm':form})"""
 			
-def addadvance(request):
+"""def addadvance(request):
 	if request.method == "POST":
 		form = AdvanceForm(request.POST)
 		if form.is_valid:
 			form.save()
 			return HttpResponse("Done!")
 	else:
-		return render(request,'src/form.html',{'form':AdvanceForm()})
+		return render(request,'src/form.html',{'form':AdvanceForm()})"""
 
-def monthlyattendance(request):
+"""def monthlyattendance(request):
 	if request.method == "POST":
 		form = MonthlyAttendanceForm(request.POST)
 		if form.is_valid:
 			form.save()
 			return HttpResponse("Yay!")
 	else:
-		return render(request,'src/form.html',{'form':MonthlyAttendanceForm()})
+		return render(request,'src/form.html',{'form':MonthlyAttendanceForm()})"""
 
 def paidamount(request):
 	if request.method == "POST":
