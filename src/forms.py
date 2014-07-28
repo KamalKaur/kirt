86f8e459b923328c6da1a4bed89a78bdaaa5ffc7
Kamal <kamal.kaur188@gmail.com>
2014-07-28 15:04:25 +0530

    Creating a ModelForm without either the 'fields' attribute or the 'exclude' attribute is deprecated

--- src/forms.py
+++ src/forms.py
@@ -5,15 +5,19 @@ from src.models import *
 class WorkerDetailForm(ModelForm):
 	class Meta:
 		model = WorkerDetail
+		fields = '__all__'
 
 class AdvanceForm(ModelForm):
 	class Meta:
 		model = Advance
+		fields = '__all__'
 
 class MonthlyAttendanceForm(ModelForm):
 	class Meta:
 		model = MonthlyAttendance
+		fields = '__all__'
 
 class PaidSalaryForm(ModelForm):
 	class Meta:
 		model = PaidSalary
+		fields = '__all__'
