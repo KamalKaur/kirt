4b715aa0cffba31d556c5bb5303f876b3da39d7f
Kamal Kaur <kamal.kaur188@gmail.com>
2014-11-01 14:28:57 +0530

    Improve the code and change events to OnInput

--- src/static/js/autosave.js
+++ src/static/js/autosave.js
@@ -2,90 +2,89 @@
 // Changed_id is introduced for separating the ids for different rows.
 
 $(document).ready(function(){
-  
-  //On changing the Days column
-  $('.days').change(function(){
-      changed_id = this.id.split("_")[1]
-      days = $('#days_' + changed_id).val();
-      request_url = "/ajaxrequest/?days=" + days + "&worker_id=" + changed_id;
-      $.ajax({
-        url: request_url,
-        success: function(){
+    //On changing the Days column
+    //$('.days').change(function(){ //Which function to use for better experience? :D
+    $('.days').on('input', function(){     
+        changed_id = this.id.split("_")[1]
+        days = $('#days_' + changed_id).val();
+        request_url = "/ajaxrequest/?days=" + days + "&worker_id=" + changed_id;
+        $.ajax({
+            url: request_url,
+            success: function(data){
             // Highlight input box border:
-            //$('#days_' + changed_id).css("border","1px solid green");
-
+            // $('#days_' + changed_id).css("border","1px solid green");
             // Change text color green:
-            $('#days_' + changed_id).css("color","#2ecc71");
-
+            if (data) $('#days_' + changed_id).css("color","#2ecc71");
             // Undo CSS but basically apply yet another CSS after some 1000 mili seconds :p
-             setTimeout( function(){
-             $('#days_' + changed_id).css("color","black");
-             },1000);
+            // setTimeout( function(){
+            // $('#days_' + changed_id).css("color","black");
+            // },1000);
+            }
+        });
+    });
+
+    $('.days').change(function(){
+        if (days==""){ 
+            alert("I didn't get a value to save, previous value is there");
         }
-      });
-  });
-
-  // On changing overtime column
-  $('.overtime').change(function(){
-    changed_id = this.id.split("_")[1]
-    overtime = $('#overtime_' + changed_id).val();
-    request_url = "/ajaxrequest/?overtime=" + overtime + "&worker_id=" + changed_id;
-    $.ajax({
-      url: request_url,
-      success: function(data){
-      // For writing on next page..
-      //document.write(data); 
-
-      }
     })
-  })
 
-  // On changing Paid Salary column    
-  $('.paid').change(function(){
-    changed_id = this.id.split("_")[1]
-    paid = $('#paid_' + changed_id).val();
-    request_url = "/ajaxrequestpaid/?paid=" + paid + "&worker_id=" + changed_id;
-    $.ajax({
-      url: request_url,
-      success: function(data){
-      alert(data);
-      }
+    // On changing overtime column
+    $('.overtime').on('input', function(){
+        changed_id = this.id.split("_")[1]
+        overtime = $('#overtime_' + changed_id).val();
+        request_url = "/ajaxrequest/?overtime=" + overtime + "&worker_id=" + changed_id;
+        $.ajax({
+            url: request_url,
+            success: function(data){
+            if (data) $('#overtime_' + changed_id).css("color","#2ecc71");
+           // For writing data on next page..
+           //document.write(data); 
+            }
+        })
     })
-  })
+
+    // On changing Paid Salary column    
+    $('.paid').on('input', function(){
+        changed_id = this.id.split("_")[1]
+        paid = $('#paid_' + changed_id).val();
+        request_url = "/ajaxrequestpaid/?paid=" + paid + "&worker_id=" + changed_id;
+        $.ajax({
+            url: request_url,
+            success: function(data){
+            if (data) $('#paid_' + changed_id).css("color","#2ecc71");
+            }
+        })
+    })
   
-  // Popup for adding advance 
-  // Is it used anymore? 
-  $('.addadvance').click(function() {
-    changed_id = this.id.split("_")[1]
-    request_url = "/ajaxrequestadvance/?worker_id=" + changed_id;
-    alert(request_url);
-    $.ajax({
-      url: request_url,
-      success: function(data){
-      alert(data);
-      }
-    })
-  })  
+    // Popup for adding advance 
+    // Is it used anymore? 
+    $('.addadvance').click(function() {
+        changed_id = this.id.split("_")[1]
+        request_url = "/ajaxrequestadvance/?worker_id=" + changed_id;
+        alert(request_url);
+        $.ajax({
+            url: request_url,
+            success: function(data){
+            alert(data);
+            }
+        })
+    })  
        
-  // Click on add advance button
-  $('.dialog').click(function () {
-    changed_id = this.id.split("_")[1]
-    request_url = "/popupadvance/?worker_id=" + changed_id;
-    alert(request_url);
-     $.ajax({
-    url: request_url,
-    success: function(data) {
-    $(".dialog").load(request_url).dialog('open');
-    }
-  });
-})
+    // Click on add advance button and it greet you with a dialog box to add more values
+    $('.dialog').click(function () {
+        changed_id = this.id.split("_")[1]
+        request_url = "/popupadvance/?worker_id=" + changed_id;
+        alert(request_url);
+        $.ajax({
+            url: request_url,
+            success: function(data) {
+            $(".dialog").load(request_url).dialog('open');
+            }
+        });
+    })
 
-//$("#dialog-form").hide();
-//$("#click").click(function(){
-//$("#dialog-form").slideToggle();
-//  });
-
-  $(".contact").click(function(ev) { // for each edit contact url
+    $(".contact").click(function(ev) { // for each edit contact url
         ev.preventDefault(); // prevent navigation
         var url = $(this).data("form"); // get the contact form url
         alert(url);
@@ -94,4 +93,5 @@ $(document).ready(function(){
         });
         return false; // prevent the click propagation
     })
+
 })	
