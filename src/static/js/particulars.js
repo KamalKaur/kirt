a98c404bdabcf0e6dfe19c1097ffbd67a8eaf1f5
Kamal Kaur <kamal.kaur188@gmail.com>
2014-11-15 01:28:25 +0530

    Redirect to main page after deleting a worker

--- src/static/js/particulars.js
+++ src/static/js/particulars.js
@@ -1,10 +1,18 @@
-    function notification(){
-        $(".notifications .messages .message").css("color","green");
-        $(".notifications").children(".messages").fadeIn(300);
-        setTimeout( function(){
-            $(".notifications").children(".messages").fadeOut(400);
-        },2000);
-    }
+
+// The function that shows the success notification 
+// and then hides after 2 seconds and redirects the 
+// user to main page
+function notification(){
+    $(".notifications .messages .message").css("color","green");
+    $(".notifications").children(".messages").fadeIn(300);
+    setTimeout( function(){
+        $(".notifications").children(".messages").fadeOut(500);
+    },2000);
+    setTimeout( function(){
+        document.location.href = '/';
+    },2000);
+    
+}
 
 function deleteworker(url){
 $.ajax({
@@ -12,9 +20,10 @@ $.ajax({
   success: function(data) {
 if(data){
   notification();
+  
   }
   else{
-  alert("not deleted");
+  alert("Not deleted");
   }
   }
 })
