e0c62a4aae84630079c337120c1737b65783a851
Kamal Kaur <kamal.kaur188@gmail.com>
2015-01-12 20:02:08 +0530

    Adds models to admin interface

--- src/admin.py
+++ src/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from src.models import *
 
 # Register your models here.
+
+admin.site.register(WorkerDetail)
+admin.site.register(MonthlyAttendance)
+admin.site.register(Advance)
+admin.site.register(PaidSalary)
+admin.site.register(Balance)
+admin.site.register(WageDescription)
